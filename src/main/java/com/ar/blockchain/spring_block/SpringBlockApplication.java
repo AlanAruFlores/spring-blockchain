4427060827f46b904d1046494e63b0dea6944fb4
AlanAruFlores <alanaruquipa242@gmail.com>
2025-04-25 23:45:51 -0300

    first commit

---
+++ src/main/java/com/ar/blockchain/spring_block/SpringBlockApplication.java
@@ -0,0 +1,13 @@
+package com.ar.blockchain.spring_block;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/blockchain/spring_block/SpringBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.blockchain.spring_block;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
